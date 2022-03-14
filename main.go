42cdfdb0a6de24432886958d9f94be8b6cffd695
ArtemTrofimushkin <artemtrofimushkin@gmail.com>
2022-03-14 21:41:25 +0300

    feat: add initial code & empty config
    Signed-off-by: ArtemTrofimushkin <artemtrofimushkin@gmail.com>

---
+++ main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	apiext "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+	"github.com/jetstack/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
+	"github.com/jetstack/cert-manager/pkg/acme/webhook/cmd"
+)
+
+var (
+	GroupName = os.Getenv("GROUP_NAME")
+)
+
+const (
+	ProviderName = "yandex"
+)
+
+func main() {
+	if GroupName == "" {
+		panic("GROUP_NAME must be specified")
+	}
+
+	// This will register our custom DNS provider with the webhook serving
+	// library, making it available as an API under the provided GroupName.
+	// You can register multiple DNS provider implementations with a single
+	// webhook, where the Name() method will be used to disambiguate between
+	// the different implementations.
+	cmd.RunWebhookServer(GroupName, &yandexDNSProviderSolver{})
+}
+
+// yandexDNSProviderSolver implements the provider-specific logic needed to
+// 'present' an ACME challenge TXT record for your Yandex Cloud DNS provider.
+// This is the implementation of `github.com/jetstack/cert-manager/pkg/acme/webhook.Solver`
+// interface.
+type yandexDNSProviderSolver struct {
+	client *kubernetes.Clientset
+}
+
+// customDNSProviderConfig is a structure that is used to decode into when
+// solving a DNS01 challenge.
+// This information is provided by cert-manager, and may be a reference to
+// additional configuration that's needed to solve the challenge for this
+// particular certificate or issuer.
+// This typically includes references to Secret resources containing DNS
+// provider credentials, in cases where a 'multi-tenant' DNS solver is being
+// created.
+type yandexDNSProviderConfig struct {
+}
+
+// Name is used as the name for this DNS solver when referencing it on the ACME
+// Issuer resource.
+func (c *yandexDNSProviderSolver) Name() string {
+	return ProviderName
+}
+
+// Present is responsible for actually presenting the DNS record with the
+// DNS provider.
+// This method should tolerate being called multiple times with the same value.
+// cert-manager itself will later perform a self check to ensure that the
+// solver has correctly configured the DNS provider.
+func (c *yandexDNSProviderSolver) Present(ch *v1alpha1.ChallengeRequest) error {
+	cfg, err := loadConfig(ch.Config)
+	if err != nil {
+		return err
+	}
+
+	// TODO: do something more useful with the decoded configuration
+	fmt.Printf("Decoded configuration %v", cfg)
+
+	// TODO: add code that sets a record in the DNS provider's console
+	return nil
+}
+
+// CleanUp should delete the relevant TXT record from the DNS provider console.
+// If multiple TXT records exist with the same record name (e.g.
+// _acme-challenge.example.com) then **only** the record with the same `key`
+// value provided on the ChallengeRequest should be cleaned up.
+// This is in order to facilitate multiple DNS validations for the same domain
+// concurrently.
+func (c *yandexDNSProviderSolver) CleanUp(ch *v1alpha1.ChallengeRequest) error {
+	// TODO: add code that deletes a record from the DNS provider's console
+	return nil
+}
+
+// Initialize will be called when the webhook first starts.
+// This method can be used to instantiate the webhook, i.e. initialising
+// connections or warming up caches.
+// Typically, the kubeClientConfig parameter is used to build a Kubernetes
+// client that can be used to fetch resources from the Kubernetes API, e.g.
+// Secret resources containing credentials used to authenticate with DNS
+// provider accounts.
+func (c *yandexDNSProviderSolver) Initialize(config *rest.Config, stop <-chan struct{}) error {
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+
+	c.client = client
+	return nil
+}
+
+// loadConfig is a small helper function that decodes JSON configuration into
+// the typed config struct.
+func loadConfig(cfgJSON *apiext.JSON) (yandexDNSProviderConfig, error) {
+	cfg := yandexDNSProviderConfig{}
+
+	if cfgJSON == nil {
+		return cfg, nil
+	}
+
+	if err := json.Unmarshal(cfgJSON.Raw, &cfg); err != nil {
+		return cfg, fmt.Errorf("error decoding solver config: %v", err)
+	}
+
+	return cfg, nil
+}
